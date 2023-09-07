f94287edb1ae890a06146ec59d2a4bba3269cb15
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2023-09-07 16:44:00 +0700

    aplikasi selesai saatnya mengirim submission

--- app/src/main/java/com/creadle/unewsid/DetailActivity.kt
+++ app/src/main/java/com/creadle/unewsid/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.creadle.unewsid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.creadle.unewsid.databinding.ActivityDetailBinding
@@ -19,7 +20,24 @@ class DetailActivity : AppCompatActivity() {
             binding.photo.setImageResource(news.photo)
             binding.nama.text = news.name
             binding.deskripsi.text = news.description
+
+            binding.btnShare.setOnClickListener{
+                shareNews(news)}
         }
     }
 
+
+    private fun shareNews(news: News){
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type="text/plain"
+            putExtra(Intent.EXTRA_SUBJECT,news.name)
+            putExtra(Intent.EXTRA_TEXT,news.description)
+
+        }
+
+        startActivity(Intent.createChooser(shareIntent,"Share News"))
+
+    }
 }
+
